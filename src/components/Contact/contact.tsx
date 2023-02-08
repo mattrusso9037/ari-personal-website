c7e28255e4119ac05c7a396580ae49f7c9b68b5e
Matt Russo <mattrusso@gmail.com>
2023-02-07 20:48:16 -0500

    feat(mobile): mobile nav menu

--- src/components/Contact/contact.tsx
+++ src/components/Contact/contact.tsx
@@ -14,7 +14,7 @@ export const Contact: React.FC = () => {
         <Section className="Contact" title={""}>
             <div className="thank_you">Thank you for taking the time to look at my portfolio! If you have any questions or would like to get in touch, please send me an email or connect with me on my social networking sites below.</div>
             <div className="social_container">
-                <div>Smiley</div>
+                <div>☺</div>
                 <ul>
                     <li onClick={() => openLink(Link.Email)}><img src={'assets/icons/email.svg'} /><span>Email Me</span></li>
                     <li onClick={() => openLink(Link.Linkedin)}><img src={'assets/icons/linkedin.svg'} /><span>LinkedIn</span></li>
