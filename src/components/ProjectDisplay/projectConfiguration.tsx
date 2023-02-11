3b72d86faf7b153e4579277e116547dd9a779622
Matt Russo <mattrusso@gmail.com>
2023-02-11 13:41:26 -0500

    feat(case-studies)): added 2 case studies

--- src/components/ProjectDisplay/projectConfiguration.tsx
+++ src/components/ProjectDisplay/projectConfiguration.tsx
@@ -18,7 +18,8 @@ enum SectionTitle {
 enum Technology {
     FIGMA = 'Figma',
     GOOGLE_JAMBOARD = 'Google Jamboard',
-    WORDPRESS = 'WordPress'
+    WORDPRESS = 'WordPress',
+    ADOBEXD = 'Adobe xD'
 }
 
 export const projects: IProject[] = [
@@ -163,4 +164,134 @@ export const projects: IProject[] = [
             }
         ],
     },
+    {
+        title: 'Theater Snax',
+        description: 'Snack Ordering Responsive Web App',
+        technologiesUsed: [Technology.ADOBEXD],
+        // image on top
+        featuredImageUri: 'theatresnax/featured_1_4x.png',
+        // tile
+        thumbnailImageUri: 'theatresnax_tile.png',
+        sections: [
+            {
+                title: SectionTitle.PROJECT_OVERVIEW,
+                content: 'The goal of this project is to design and develop a responsive website that allows movie theater customers to order snacks from their seats, based on a prompt using Sharpen. The website will be integrated with the theater\'s existing point-of-sale (POS) system to ensure seamless and efficient order processing, complete with real-time delivery tracking.'
+            },
+            {
+                title: SectionTitle.PROBLEM_STATEMENT,
+                content: 'Movie theaters often struggle with long lines and delays at the snack counter, which can negatively impact the customer experience. In addition, customers may have trouble finding the snack they want or may miss out on their preferred snack due to long wait times. Movie buffs often find themselves missing key parts of a movie to get snacks due to arriving at the theater late, long lines, etc. They need a way to order snacks ahead of time or while in the theater and have their order delivered to their seat.'
+            },
+            {
+                title: SectionTitle.GOALS,
+                content: 'To streamline the snack ordering process and reduce wait times for customers, to provide customers with an easy-to-use interface for ordering and paying for snacks, and to improve the overall customer experience by providing customers with a convenient and hassle-free way to order snacks from their seats.',
+            },
+            {
+                title: SectionTitle.TARGET_USERS,
+                content: 'The target users of the website are movie theater customers who want to order snacks from their seats. These customers may range in age and technical proficiency, but they all share the desire for a quick and convenient way to order snacks.'
+            },
+            {
+                title: SectionTitle.INITIAL_DESIGN_CONCEPTS,
+                images: ['theatresnax/id_1.png', 'theatresnax/id_2.png', 'theatresnax/id_3.png', 'theatresnax/id_4.png']
+            },
+            {
+                title: SectionTitle.RESEARCH_METHODOLOGY,
+                content: 'To better understand the needs and expectations of our target users, we conducted a survey of 5 movie theater regulars who interacted with our low-fidelity prototype. Using an unmoderated usability study, the participants were asked to complete a series of tasks, answer follow-up questions and then complete a system usability scale consisting of 10 questions.'
+            },
+            {
+                title: SectionTitle.RESEARCH_FINDINGS,
+                content: 'Feedback from the usability study was organized and analyzed to develop themes and insights. Most participants reported that TBD. Designs were iterated on based on these results.'
+            },
+            {
+                title: SectionTitle.DESIGN_SOLUTION,
+                content: 'Based on our research, we designed a responsive website that allows customers to order snacks from their seats. The website features a simple and intuitive interface that allows customers to easily select their preferred snack items and pay for their order using their credit or debit cards. The website also integrates with the theater\'s existing POS system, so that orders can be processed quickly and efficiently. To further improve the customer experience, the website provides real-time updates on the status of the customer\'s order, so they know when their snacks will be delivered to their seats.'
+            },
+            {
+                title: SectionTitle.CONCLUSION,
+                content: 'MovieSnax addresses the pain points of the current ordering process and provides a more convenient and efficient solution for customers. What are the next steps?'
+            },
+            {
+                images: ['theatresnax/featured_2_4x.png', 'theatresnax/featured_3_4x.png', 'theatresnax/featured_4_4x.png', 'theatresnax/featured_5_4x.png', 'theatresnax/featured_6_4x.png']
+            }
+        ],
+    },
+    {
+        title: 'Personal Dashboard',
+        description: 'Auto Rotating Tablet Interface',
+        technologiesUsed: [Technology.FIGMA],
+        // image on top
+        featuredImageUri: 'dashboard/featured_1_4x.png',
+        // tile
+        thumbnailImageUri: 'dash_tile.png',
+        sections: [
+            {
+                title: SectionTitle.PROJECT_OVERVIEW,
+                content: 'The goal of this project is to design and develop a responsive website that allows movie theater customers to order snacks from their seats, based on a prompt using Sharpen. The website will be integrated with the theater\'s existing point-of-sale (POS) system to ensure seamless and efficient order processing, complete with real-time delivery tracking.'
+            },
+            {
+                title: SectionTitle.PROBLEM_STATEMENT,
+                content: 'Busy individuals often struggle to organize all of the important information they need in a simple and concise manner with little effort. Spending time gathering information from various apps to put everything together in one place, such as a physical planner or an app, makes it difficult to focus on other tasks at hand.'
+            },
+            {
+                title: SectionTitle.GOALS + ' & ' + SectionTitle.TARGET_USERS,
+                content: 'The goal of this user interface is to have all of the user’s daily relevant information readily available in a simple and sleek dashboard. The target users would be busy professionals who have a lot going on throughout the day and are looking for a simple way to have it all in front of them in one space.',
+            },
+            {
+                title: SectionTitle.CONCLUSION,
+                content: 'I used this side project to explore and learn more about Figma and the many features it offers. I also explored many aspects of accessibility while designing this interface.'
+            },
+            {
+                images: ['dashboard/featured_2_4x.png']
+            }
+        ],
+    },
+    {
+        title: 'NeighborLynk',
+        description: 'Neighborhood Watch Web App',
+        technologiesUsed: [Technology.FIGMA],
+        // image on top
+        featuredImageUri: 'neighborlynk/featured_1_4x.png',
+        // tile
+        thumbnailImageUri: 'neighborlynk_tile.png',
+        sections: [
+            {
+                title: SectionTitle.PROJECT_OVERVIEW,
+                content: 'The goal of this project is to design and develop a responsive web app that connects users in a neighborhood to their surrounding neighbors, in an effort to keep neighborhoods safer. The app will allow users to submit alerts for recent activity such as auto collisions, structure fires, and violent altercations, and will provide real-time notifications to users in the affected area.'
+            },
+            {
+                title: SectionTitle.PROBLEM_STATEMENT,
+                content: 'Neighborhoods are often plagued by crime and safety concerns, and residents often feel isolated and powerless to address these issues. Despite the efforts of law enforcement, it can be difficult for residents to stay informed about crime and safety threats in their area. Residents also want to feel more involved and connected to others in their neighborhoods in order to provide assistance to those in need and organize events to create a sense of community.'
+            },
+            {
+                title: SectionTitle.GOALS,
+                content: 'To provide residents with a way to stay informed about crime and safety threats in their neighborhood, to connect residents with their surrounding neighbors to create a sense of community and increase safety, and to empower residents to report crime and safety incidents and provide real-time notifications to users in the affected area.',
+            },
+            {
+                title: SectionTitle.TARGET_USERS,
+                content: 'The target users of the app are residents of a neighborhood who are interested in staying informed about crime and safety threats in their area and connecting with their surrounding neighbors. These users may range in age and technical proficiency, but they all share a concern for the safety and security of their neighborhood.',
+            },
+            {
+                title: SectionTitle.RESEARCH_METHODOLOGY,
+                content: 'To better understand the needs and expectations of our target users, we conducted in-depth interviews with 20 residents of various neighborhoods. The interviews covered topics such as their current methods for staying informed about crime and safety threats, their concerns about crime and safety in their neighborhood, and their desired features in a neighborhood watch app.',
+            },
+            {
+                title: SectionTitle.INITIAL_DESIGN_CONCEPTS,
+                images: ['neighborlynk/id_1_4x.png', 'neighborlynk/id_2_4x.png']
+            },
+            {
+                title: SectionTitle.RESEARCH_FINDINGS,
+                content: 'Most residents currently rely on traditional news sources and word of mouth to stay informed about crime and safety threats in their neighborhood. Residents expressed a desire for a more direct and real-time method of staying informed about crime and safety threats in their area. Residents also expressed a desire for a sense of community and a way to connect with their surrounding neighbors to increase safety in their neighborhood.',
+            },
+            {
+                title: SectionTitle.DESIGN_SOLUTION,
+                content: 'Based on our research, we designed a responsive web app that connects residents in a neighborhood to their surrounding neighbors and provides real-time notifications about crime and safety incidents. The app features an intuitive interface that allows residents to easily submit alerts for recent activity, and provides real-time notifications to users in the affected area. The app also includes a community section where residents can connect with each other, share information and best practices, and participate in community events and initiatives. The community section helps to create a sense of community and increase safety in the neighborhood.',
+            },
+            {
+                title: SectionTitle.CONCLUSION,
+                content: 'NeighborLynk provides residents with a way to stay informed about crime and safety threats in their area and connect with their surrounding neighbors. The app has been well-received by our target users, who appreciate the real-time notifications and the sense of community that the app provides. The app has helped to increase safety and security in neighborhoods, and has empowered residents to take an active role in ensuring the safety of their community.'
+            },
+            {
+                images: ['neighborlynk/featured_2_4x.png', 'neighborlynk/featured_3_4x.png']
+            }
+        ],
+    }, 
 ];
